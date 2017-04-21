4f296d980c3aa2520ffcae49e6efd06559af629b
mDuo13 <mduo13@gmail.com>
2017-04-20 20:09:30 -0700

    Update bot

--- bot.py
+++ bot.py
@@ -21,12 +21,12 @@ def activates_bot(msg):
 
 def tx_lookup(tx_hash, verbose):
     try:
-        tx_json = txsplain.fetch(tx_hash)["result"]
+        tx_json = txsplain.tx(tx_hash)["result"]
         s = "https://api.ripple.com/v1/transactions/"+tx_hash+"\n"
         s += txsplain.splain(tx_json, verbose)
     except KeyError:
         s = "Couldn't find transaction %s." % tx_hash
-        
+
     return s
 
 while True:
